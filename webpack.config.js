6f852256d6d086487fc4505b3bd29821300d7c1c
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2018-07-18 17:39:04 -0400

    Deleted folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const webpack = require("webpack");
-const bundlePath = path.resolve(__dirname, "./dist");
+const bundlePath = path.resolve(__dirname, "dist");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, "./src/index.html"),
-  filename: "./index.html"
+  template: path.join(__dirname, "src/index.html"),
+  filename: "index.html"
 });
 
 module.exports = {
-  entry: path.join(__dirname, "./src/index.js"),
+  entry: path.join(__dirname, "src/index.js"),
   module: {
     rules: [
       {
