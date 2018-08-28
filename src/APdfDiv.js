f07b9669d2e699028a9a6218e195246fad9827b6
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2018-08-28 14:14:54 -0400

    Changed pdfDiv back to clickable div

--- src/APdfDiv.js
+++ src/APdfDiv.js
@@ -28,14 +28,11 @@ export default class APdfDiv extends Component{
 		return(
 
 			<div>
-				Changed again!
+				<a class = "aPdfDiv" href = {"https://rawgit.com/NumaKarolinski/PersonalWebsite/websiteVersion1/src/PDFs/" + this.props.fileName} download={this.props.downloadName} onClick = {(e) => this.aButtonDivClick(e)}>
+					<span id = {this.props.fileName} class = "notesTextBox">{this.props.downloadName}</span>
+				</a>
 			</div>			
 
 		);
 	}
 }
-
-
-/*<a class = "aPdfDiv" href = {"https://cdn.rawgit.com/NumaKarolinski/PersonalWebsite/websiteVersion1/src/PDFs" + this.props.fileName} download={this.props.downloadName} onClick = {(e) => this.aButtonDivClick(e)}>
-				<span id = {this.props.fileName} class = "notesTextBox">{this.props.downloadName}</span>
-			</a>*/
