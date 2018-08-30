f80f72950cd26cefdd337e817a3c023d14a5d935
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2018-08-30 09:13:02 -0400

    Updating website

--- updatedWebsite.sh
+++ updatedWebsite.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 if cp ./dist/index.html ./dist/bundle.js ./.. ; then
-
 	printf "Files are copied to Desktop!"
 else
 	printf "Either you are in the wrong directory, or /dist has been rearranged."
 fi
+npm run build
+git add -A
+git commit -m "Updating Online Website"
+git push origin websiteVersion1
+git checkout gh-pages
+rm bundle.js
+rm index.html
+cp ./../index.html ./../bundle.js .
+git add -A
+git commit -m "Updating Online Website"
+git push origin gh-pages
+git checkout websiteVersion1
+printf "Your website is updated!"+\n
+printf "You might want to merge the master branch (and fix merge conflicts)."
