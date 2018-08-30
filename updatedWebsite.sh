061c9c4c45d82347cdbf6a423db9170782823a38
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2018-08-30 14:04:46 -0400

    Updating scripts

--- updatedWebsite.sh
+++ updatedWebsite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if cp ./dist/index.html ./dist/bundle.js ./.. ; then
+if cp ./dist/index.html ./dist/bundle.js ./updatedWebsite.sh ./.. ; then
 	printf "Files are copied to Desktop!"
 else
 	printf "Either you are in the wrong directory, or /dist has been rearranged."
@@ -11,7 +11,7 @@ git push origin websiteVersion1
 git checkout gh-pages
 rm bundle.js
 rm index.html
-cp ./../index.html ./../bundle.js .
+cp ./../index.html ./../bundle.js ./../updatedWebsite.sh .
 git add -A
 git commit -m "Updating Online Website"
 git push origin gh-pages
@@ -20,3 +20,4 @@ printf "Your website is updated!\n"
 printf "You might want to merge the master branch (and fix merge conflicts)."
 rm ./../bundle.js
 rm ./../index.html
+rm ./../updatedWebsite.sh
