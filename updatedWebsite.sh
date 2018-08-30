54348e61b00e135af834887ca1a0e02e7643b855
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2018-08-30 00:22:09 -0400

    Added websiteUpdated.sh script to gh-pages

---
+++ updatedWebsite.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if cp ./dist/index.html ./dist/bundle.js ./.. ; then
+
+	printf "Files are copied to Desktop!"
+else
+	printf "Either you are in the wrong directory, or /dist has been rearranged."
+fi
