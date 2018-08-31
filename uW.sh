d605abd703ebbe6b7574ba448fc2f029084e8c65
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2018-08-30 21:58:41 -0400

    Updated WebsiteVersion1 to master

--- uW.sh
+++ uW.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if cp ./dist/index.html ./dist/bundle.js ./.. ; then
+if cp ./dist/index.html ./dist/bundle.js ./uW.sh ./.. ; then
 	printf "Files are copied to Desktop!"
 else
 	printf "Either you are in the wrong directory, or /dist has been rearranged."
@@ -11,10 +11,13 @@ git push origin websiteVersion1
 git checkout gh-pages
 rm bundle.js
 rm index.html
-cp ./../index.html ./../bundle.js .
+cp ./../index.html ./../bundle.js ./../uW.sh .
 git add -A
 git commit -m "Updating Online Website"
 git push origin gh-pages
 git checkout websiteVersion1
-printf "Your website is updated!"
+printf "Your website is updated!\n"
 printf "You might want to merge the master branch (and fix merge conflicts)."
+rm ./../bundle.js
+rm ./../index.html
+rm ./../uW.sh
