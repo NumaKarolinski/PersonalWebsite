a003e17263a353d4bdc10a4d3562cf2dd2e8f5e2
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2018-09-01 16:32:42 -0400

    Script Fix #5

---
+++ uW2.sh
@@ -0,0 +1,11 @@
+rm ./bundle.js
+rm ./index.html
+cp ./../index.html ./../bundle.js .
+rm ./../bundle.js
+rm ./../index.html
+git add -A
+git commit -m "Updating Online Website"
+git push origin gh-pages
+printf "Your website should is updated if there were no errors!\n"
+printf "You might want to merge the master branch (and fix merge conflicts)."
+printf "You can then `git checkout _branch_` to leave this branch."
