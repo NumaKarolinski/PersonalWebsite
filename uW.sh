76de446512cd801878e9f8c7dc59688a2b1feffc
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2020-09-13 12:19:46 -0400

    Updating Online Website

--- uW.sh
+++ uW.sh
@@ -5,8 +5,13 @@ else
 	printf "Either you are in the wrong directory, or /dist has been rearranged."
 fi
 npm run build
+commitString=''
+while [ -z "$commitString" ]
+do
+	read -p 'Comment for commit to websiteVersion1.2: ' commitString
+done
 git add -A
-git commit -m "Updating Online Website"
+git commit -m $commitString
 git push origin websiteVersion1.2
 git checkout gh-pages
 rm bundle.js
