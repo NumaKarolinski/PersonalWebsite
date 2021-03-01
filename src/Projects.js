ba8bafc15412d0e2f11026b4e6a7e7804a53c7ef
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2021-03-01 17:37:36 -0500

    Added another projectdiv for my website called nuschedule.

--- src/Projects.js
+++ src/Projects.js
@@ -11,7 +11,6 @@ import AProjectDiv from './AProjectDiv.js';
 export default class Projects extends Component{
 
 	constructor(props){
-
 		super(props);
 	}
 
@@ -32,7 +31,9 @@ export default class Projects extends Component{
                         <AProjectDiv fileName = "parallaxEffectGray.png" projectName = "cita2019" width = {448} height = {264} clickText = "Read My Publication For More Information!" link = "https://arxiv.org/pdf/2006.02441.pdf" description = "I worked alongside Dr. Wei Zhu from the Canadian Institute for Theoretical Astrophysics in 2019 as an undergraduate researcher. I analyzed OGLE-III data for black holes using a new gravitational microlensing technique." />
                             
                         <AProjectDiv fileName = "albedoMapWebsite.png" projectName = "mecha2020" width = {424.4} height = {264} clickText = "Take a look at my stan code!" link = "https://github.com/NumaKarolinski/exoplanet_inverse_problem_stan_toy_model" description = "In the summer of 2020 I worked with Prof. Nicolas Cowan from McGill University. I investigated 3 programs that solve the exoplanet inverse problem. I ended the summer by writing a stan program that solves the inverse problem with a Hamiltonian Monte Carlo simulation." />
-                            
+
+			<AProjectDiv filename = "nufavicon.png" projectName = "nuschedule2021" width = {264} height = {264} clickText = "Check out nuschedule!" link = "https://nuschedule.com" description = "We always know what we want to do, but scheduling gets annoying, and strict schedules get monotonous. Define your events, and let my website generate the schedule for you. Include randomization features to make each day unique." />
+
                     </div>
 				</BodyDiv>
 			</BackgroundDiv>
