4d1f12ae10aaa9aeeabb13609e18d5a36df6db5d
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2021-03-02 19:20:17 -0500

    Updating Online Website

--- uW.sh
+++ uW.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+npm run build
 if cp ./dist/index.html ./dist/bundle.js ./uW.sh ./.. ; then
 	printf "Files are copied to Desktop!"
 else
 	printf "Either you are in the wrong directory, or /dist has been rearranged."
 fi
-npm run build
 commitString=''
 while [ -z "$commitString" ]
 do
